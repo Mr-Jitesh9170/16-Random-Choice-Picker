b7b0e7028cec494b435cee644c947e38959e4401
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2024-01-11 23:29:54 +0530

    " 'enter' button features added"

--- src/RandomChoicePicker/random.jsx
+++ src/RandomChoicePicker/random.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import "./random.scss"
 export const RandomChoicePicker = () => {
   const [data, setData] = useState("hello, Jitesh!!");
+  const [save, setSave] = useState(data);
   return (
     <div className="random-choice-picker-container">
       <h2 className="paragraph">
@@ -10,11 +11,19 @@ export const RandomChoicePicker = () => {
       </h2>
       <textarea name="" id="" placeholder="enter your choices here..." value={data} cols="50" rows="8" onChange={(e) => {
         setData(e.target.value)
+      }} onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          setSave(data)
+        }
       }}></textarea>
       <div className="choices">
         {
-          data.split(',').map((_, i) => {
-            return <span key={i}>{_.trim()}</span>
+          save.split(',').map((_, i) => {
+            if (_ !== " ") {
+              return <span key={i} onClick={() => {
+
+              }}>{_.trim()}</span>
+            }
           })
         }
       </div>
@@ -23,4 +32,3 @@ export const RandomChoicePicker = () => {
 }
 
 
-const Choices = [7, 8, 9, 10]
